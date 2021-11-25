46090836dde5caa524e9a039f8df8e037c904426
Riccardofal <91559110+Riccardofal@users.noreply.github.com>
2021-11-25 09:40:10 +0100

    aggiornamento

--- src/main/java/it/epicode/beservice/common/Commons.java
+++ src/main/java/it/epicode/beservice/common/Commons.java
@@ -17,44 +17,45 @@ import it.epicode.beservice.model.Role;
 import it.epicode.beservice.repository.RoleRepository;
 import it.epicode.beservice.service.ProvinciaService;
 
-//@Component
+@Component
 public class Commons implements CommandLineRunner {
 
 	@Autowired
 	ComuneService comuneService;
-	
+
 	@Autowired
 	ProvinciaService provinciaService;
-	
+
 	@Autowired
 	RoleRepository roleRepo;
+
 	
-	private static final String PATH_PROVINCIA="C:\\Users\\ricca\\Downloads\\province-italiane (1).csv";
-	private static final String PATH_COMUNE="C:\\Users\\ricca\\Downloads\\comuni-italiani (1).csv";
-	
+
 	@Override
 	public void run(String... args) throws Exception {
-		
-		Role role=new Role();
-			role.setEroles(Erole.ROLE_ADMIN);
-			this.roleRepo.save(role);
-			Role roleUser=new Role();
-			roleUser.setEroles(Erole.ROLE_USER);
-			this.roleRepo.save(roleUser);
+
+		Role role = new Role();
+		role.setEroles(Erole.ROLE_ADMIN);
+		this.roleRepo.save(role);
+		Role roleUser = new Role();
+		roleUser.setEroles(Erole.ROLE_USER);
+		this.roleRepo.save(roleUser);
 		recuperoDaCsvProvincia();
 		recuperoDaCsvComune();
-		
+
 	}
 
 	private void recuperoDaCsvComune() {
-		try 
-			(BufferedReader br= new BufferedReader(new FileReader(PATH_COMUNE))){
+		String dir = System.getProperty("user.dir");
+		String path = "\\src\\main\\resources\\static\\comuni-italiani (1).csv";
+		String comp = (dir + path).trim();
+		try (BufferedReader br = new BufferedReader(new FileReader(comp))) {
 			String line;
-			while ((line=br.readLine()) != null) {
-				String[]part=line.split(";");
-				Comune comune= new Comune();
+			while ((line = br.readLine()) != null) {
+				String[] part = line.split(";");
+				Comune comune = new Comune();
 				comune.setNome(part[2]);
-				Provincia prov =provinciaService.findByNome(part[3]);
+				Provincia prov = provinciaService.findByNome(part[3]);
 				comune.setProvincia(prov);
 				comuneService.addComune(comune);
 			}
@@ -66,28 +67,23 @@ public class Commons implements CommandLineRunner {
 	}
 
 	private void recuperoDaCsvProvincia() {
-		try 
-			(BufferedReader br= new BufferedReader(new FileReader(PATH_PROVINCIA))){
+		String dir = System.getProperty("user.dir");
+		String path = "\\src\\main\\resources\\static\\province-italiane (1).csv";
+		String comp = (dir + path).trim();
+		try (BufferedReader br = new BufferedReader(new FileReader(comp))) {
 			String line;
-			while((line=br.readLine()) != null) {
-				String[]part=line.split(";");
-				Provincia prov= new Provincia();
+			while ((line = br.readLine()) != null) {
+				String[] part = line.split(";");
+				Provincia prov = new Provincia();
 				prov.setSigla(part[0]);
 				prov.setNome(part[1]);
 				provinciaService.addProvincia(prov);
 			}
-			}catch (FileNotFoundException e) {
-				e.printStackTrace();
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
 		}
-	
 	}
 
-	
-	
-	
-	
-	
-
+}
